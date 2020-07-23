edc790d67b2d2af5bcd8f985eda0355b545e7fc9
Christopher O'Keefe <chrisokeefe4@gmail.com>
2020-07-23 14:03:56 -0400

    Added .gitignore, new SECRET_KEY for production

--- website/settings.py
+++ website/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9#995q1@j2w+fb0=cz8_9mrore7cbt4j0sb(bz6a@s-v*3v=i5'
+
+with open(os.path.join(BASE_DIR, 'etc\\secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
