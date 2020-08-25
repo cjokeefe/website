ee2319d08813902527579d4055dcc6b44071ffe0
cjokeefe <chrisokeefe4@gmail.com>
2020-08-25 12:29:56 -0400

    Added misc. page to home with random gradient generator

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home-page'),
     path('about/', views.about, name='home-about'),
     path('projects/', views.projects, name='home-projects'),
+    path('misc/', views.misc, name='home-misc'),
+    path('misc/gradientGen/', views.gradient, name='home-misc-gradient'),
 ]
